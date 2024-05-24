eedfbebc5c56c95fbcd6d299a048bc947d568f8f
Chen <2934570984@qq.com>
2024-05-24 09:12:04 +0800

    feat: 路由常见功能，query param body

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
   @Get('')
-  findAll() {
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
     return {
       list: [1, 2, 3],
       count: 10,
+      keyword,
+      page,
+      pageSize,
     };
   }
 
-  
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    console.log(id, questionDto);
+
+    return {
+      id,
+      questionDto,
+    };
+  }
+
   @Get('test')
   test(): string {
     return 'question test!';
